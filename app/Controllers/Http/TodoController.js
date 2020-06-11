f4af76c2cfb0865fef75e3b4ed4c24afe5077fe8
LethalPants <48921721+LethalPants@users.noreply.github.com>
2020-06-12 00:51:44 +0300

    Initial commit with CRUD todos

---
+++ app/Controllers/Http/TodoController.js
@@ -0,0 +1,153 @@
+"use strict";
+const Todo = use("App/Models/Todo");
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with todos
+ */
+class TodoController {
+  /**
+   * Show a list of all todos.
+   * GET todos
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {
+    try {
+      const todos = await Todo.all();
+
+      return response.status(200).json({
+        todos: todos.toJSON(),
+      });
+    } catch (e) {
+      console.log(e);
+      return response.status(e.status).json({
+        message: "Something went wrong.",
+        error: e,
+      });
+    }
+  }
+
+  /**
+   * Render a form to be used for creating a new todo.
+   * GET todos/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create({ request, response, view }) {}
+
+  /**
+   * Create/save a new todo.
+   * POST todos
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {
+    try {
+      const reqItem = request.only(["todo"]).todo;
+      console.log(reqItem);
+      const todo = await Todo.create({
+        todo: reqItem,
+      });
+
+      return response.status(201).json({
+        message: "Successfully added item.",
+        item: todo,
+      });
+    } catch (err) {
+      console.log(err);
+      return response.status(err.status).json({
+        message: "Something went wrong.",
+        error: err,
+      });
+    }
+  }
+
+  /**
+   * Display a single todo.
+   * GET todos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {}
+
+  /**
+   * Render a form to update an existing todo.
+   * GET todos/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit({ params, request, response, view }) {}
+
+  /**
+   * Update todo details.
+   * PUT or PATCH todos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, session, request, response }) {
+    try {
+      const reqItem = request.only(["todo", "completed"]);
+      const todo = await Todo.findOrFail(params.id);
+      todo.todo = reqItem.todo;
+      reqItem.todo ? (todo.todo = reqItem.todo) : null;
+      reqItem.completed ? (todo.completed = reqItem.completed) : null;
+      await todo.save();
+      return response.status(200).json({
+        message: "Successfully updated item.",
+        item: todo,
+      });
+    } catch (err) {
+      console.log(err);
+      return response.status(err.status).json({
+        message: "Something went wrong.",
+        error: err.message,
+      });
+    }
+  }
+
+  /**
+   * Delete a todo with id.
+   * DELETE todos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, session, request, response }) {
+    try {
+      const todo = await Todo.findOrFail(params.id);
+      await todo.delete();
+      response.status(200).json({
+        message: "Item successfully deleted.",
+      });
+    } catch (err) {
+      console.log(err);
+      return response.status(err.status).json({
+        message: "Something went wrong.",
+        error: err,
+      });
+    }
+  }
+}
+
+module.exports = TodoController;
---
+++ start/routes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use("Route");
+
+Route.group(() => {
+  Route.get("/", "TodoController.index").as("todos.index");
+
+  Route.post("/new", "TodoController.store").as("todos.store");
+
+  Route.patch("/:id/edit", "TodoController.update").as("todos.update");
+
+  Route.delete("/:id/delete", "TodoController.destroy").as("todos.delete");
+}).prefix("api/v1/todos");
